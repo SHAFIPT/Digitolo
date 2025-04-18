5b2b5267757242c19e3e96a303b2d06497b855a8
SHAFIPT <shafipt077@gmail.com>
2025-04-18 10:08:40 +0530

    feat : cores setted

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,8 +15,8 @@ connectDB()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
-app.use(cors({   
-  origin: ['http://localhost:3000'],
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://digitolo.vercel.app'],
   credentials: true
 }));
 
